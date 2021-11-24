a01e156760626ccf68b0c6068839f2b25b75bcdf
Kelvin Munro <kmunro4@ford.com>
2021-11-24 08:52:53 +0000

    attempted refactor of Swift Kata, pulled update function out and added tests to try and test all outcomes

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -5,28 +5,46 @@ public class GildedRose {
         self.items = items
     }
     
+    func updateQualityIncrease(placement: Int) -> Int {
+        if placement >= 50 {
+            return 0
+        } else {
+            let temp = placement + 1
+            return temp
+        }
+    }
+    
+    func updateQualitySubtract(placement: Int) -> Int {
+        if placement <= 0 {
+            return 0
+        } else {
+            let temp = placement - 1
+            return temp
+        }
+    }
+    
     public func updateQuality() {
         for i in 0 ..< items.count {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (items[i].quality > 0) {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+                        items[i].quality = updateQualityIncrease(placement: items[i].quality)
                     }
                 }
             } else {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                    items[i].quality = updateQualityIncrease(placement: items[i].quality)
                     
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                                items[i].quality = updateQualityIncrease(placement: items[i].quality)
                             }
                         }
                         
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                                items[i].quality = updateQualityIncrease(placement: items[i].quality)
                             }
                         }
                     }
@@ -42,15 +60,16 @@ public class GildedRose {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].quality > 0) {
                             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+                                items[i].quality = updateQualitySubtract(placement: items[i].quality)
                             }
                         }
                     } else {
+//                        Need to check what this is getting at below
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                        items[i].quality = updateQualityIncrease(placement: items[i].quality)
                     }
                 }
             }
--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -2,15 +2,84 @@
 import XCTest
 
 class GildedRoseTests: XCTestCase {
+    
+    let items = [
+        Item(name: "foo", sellIn: 0, quality: 0),
+        Item(name: "Aged Brie", sellIn: 2, quality: 10),
+        Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 10),
+        Item(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
+        Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 80),
+        Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80),
+        Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20),
+        Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49),
+        Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49),
+        // this conjured item does not work properly yet
+        Item(name: "Conjured Mana Cake", sellIn: 3, quality: 6)]
 
+    
     func testFoo() throws {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
         let app = GildedRose(items: items)
         app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
+        XCTAssertEqual(app.items[0].name, "foo")
     }
+    
+    func testAgedBrieSellIn() throws {
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[1].sellIn, 1)
+    }
+    
+    func testAgedBrieQuality() throws {
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[1].quality, 11)
+    }
+    
+    func testDexterityVest() throws {
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[2].quality, 11)
+    }
+    
+    func testElixerOfTheMongoose() throws {
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[3].quality, 8)
+    }
+    
 
+    func testSulfurasHandOfRagnaros() throws {
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[4].quality, 80)
+    }
+    
+    func testBackstagePassSellIn15() throws {
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[6].quality, 21)
+    }
+    
+    func testBackstagePassSellIn10() throws {
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[7].quality, 50)
+    }
+    
+    func testBackstagePassSellIn5() throws {
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[8].quality, 50)
+    }
+    
     static var allTests = [
         ("testFoo", testFoo),
+        ("testAgedBrieQuality", testAgedBrieQuality),
+        ("testDexterityVest", testDexterityVest),
+        ("testElixerOfTheMongoose", testElixerOfTheMongoose),
+        ("testBackstagePassSellIn15", testBackstagePassSellIn15),
+        ("testBackstagePassSellIn10", testBackstagePassSellIn10),
+        ("testBackstagePassSellIn5", testBackstagePassSellIn5)
+//        ("testSulfurasHandOfRagnaros", testSulfurasHandOfRagnaros)
     ]
 }
